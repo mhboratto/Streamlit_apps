7dfcc3016b2a5f021017c251e42089327a874079
Miguel Boratto <99510944+mhboratto@users.noreply.github.com>
2022-11-30 22:15:49 +0000

    Update aula_streamlit.py

--- aula_streamlit.py
+++ aula_streamlit.py
@@ -1,19 +1,12 @@
 import streamlit as st
-#import plost
 import pandas as pd
 
 #st.sidebar()
-st.title("Welcome Miguel")
+st.title("Portfolio")
 
-# df = {'time':[0,1,2,3,4,5,6,7,8,9],
-#       'a':[3,4,5,6,7,5,4,3,1,0],
-#       'b':[1,5,6,7,9,4,2,7,8,4]
-#       }
-# df = pd.DataFrame(df)
-# plost.line_chart(
-#   df,
-#   x='time',
-#   y=('a', 'b'),
-#   legend='top',
-#   #pan_zoom='minimap',  # 👈 This is magic!
-# )
+st.markdown('''## Formation
+### Physicist and PhD in Materials Science
+
+## Programming Experience
+### Knowledge in Python, focused in Data Analysis
+''')
