cbd704b30392eabf2aa0f6a51075cdf761ea7984
Miguel Boratto <99510944+mhboratto@users.noreply.github.com>
2022-11-30 21:35:20 +0000

    Add files via upload

---
+++ aula_streamlit.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 29 19:52:37 2022
+
+@author: Miguel
+"""
+
+import streamlit as st
+import plost
+import pandas as pd
+
+#st.sidebar()
+st.title("Welcome Miguel")
+
+df = {'time':[0,1,2,3,4,5,6,7,8,9],
+      'a':[3,4,5,6,7,5,4,3,1,0],
+      'b':[1,5,6,7,9,4,2,7,8,4]
+      }
+df = pd.DataFrame(df)
+plost.line_chart(
+  df,
+  x='time',
+  y=('a', 'b'),
+  legend='top',
+  #pan_zoom='minimap',  # 👈 This is magic!
+)
